ba928fe7a4623aec5db7e66b8294267c9b7d5a6f
Saiful Islam <saifsust@users.noreply.github.com>
2017-09-16 22:49:06 +0600

    push and pop test

--- collections/deque.java
+++ collections/deque.java
@@ -1,6 +1,8 @@
 import java.util.*;
 public class deque{
-
+/*
+ like as stack
+*/
 public static void main(String[] args)
 {
 	Scanner read = new Scanner(System.in);
@@ -11,13 +13,13 @@ public static void main(String[] args)
       int num = read.nextInt();
       for(int i=1;i<=num;i++)
       {
-        dq.add(i*100);
+        dq.push(i*100);
       }
 
 
       while(!dq.isEmpty())
       {
-        System.out.println(dq.poll());
+        System.out.println(dq.pop());
       }
 
 	}
