acccb571a4909fb94cbca2c793e37da6835b993b
Saiful Islam <saifsust@users.noreply.github.com>
2017-09-18 00:20:19 +0600

    2 dimensional linked List

---
+++ collections/linkedList.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class linkedList{
+	
+
+
+	public static void main(String[] args) throws Exception
+	{
+
+		LinkedList<Integer> list[] = new LinkedList[100];
+		Scanner read = new Scanner(System.in);
+		while(read.hasNext())
+		{
+          int num = read.nextInt();
+          for(int i=0;i<num;i++)
+          	list[i] = new LinkedList<Integer>();
+          for(int i=0;i<num;i++)
+          {
+          	for(int j=1;j<num;j++)
+          		list[i].add(j*1000);
+
+          }
+            for(int i=0;i<num;i++)
+            {
+            	for(int j=0;j<list[i].size();j++)
+            		System.out.print(list[i].get(j)+" ");
+            	System.out.println();
+            }
+
+         
+		}
+
+	}
+}
