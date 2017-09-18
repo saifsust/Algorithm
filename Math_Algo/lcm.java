2978721f638d58f73ea45653fcca4ae7e8509a97
Saiful Islam <saifsust@users.noreply.github.com>
2017-09-18 14:05:41 +0600

    least common multiple

---
+++ Math_Algo/lcm.java
@@ -0,0 +1,34 @@
+
+import java.util.*;
+import java.lang.*;
+
+ class Main{
+
+private static int GCD(int a,int b)
+{
+   if(a==0) return b;
+   return GCD(b%a,a);
+
+}
+
+public static void main(String[] args) throws Exception
+{
+   Scanner read = new Scanner(System.in);
+   while(read.hasNext())
+   {
+
+   	  int a=read.nextInt();
+   	  int b=read.nextInt();
+        int numB=Math.max(a,b);
+        int numA= Math.min(a,b);
+        int gcd = GCD(numA,numB);
+        int lcm = (a*b)/gcd;
+   	  System.out.println(lcm);
+
+     
+   }
+
+   read.close();
+}
+
+}
