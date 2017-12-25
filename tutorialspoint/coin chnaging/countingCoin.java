dfb36f29ff6d2b76045086dbaf8ba0aaa80c117a
Saiful Islam <saifsust@users.noreply.github.com>
2017-12-26 01:33:02 +0600

    OPTIMAL + RECURSIVE ALGORITHM

--- tutorialspoint/coin chnaging/countingCoin.java
+++ tutorialspoint/coin chnaging/countingCoin.java
@@ -4,9 +4,10 @@ import java.io.*;
 class Main
 {
 
-    private final int N = 1000;
+    private final int N = 10000;
     private int[][] matrix = new int[N][N];
     private int[] coins;
+    private int counter ;
     private final int INF = 1000000000;
 
     private   void coinChange() throws Exception
@@ -26,18 +27,20 @@ class Main
             //  System.out.println(numOfCoins);
 
             coins[0] = 0;
-            for(int i = 1; i <= numOfCoins; i++)
+            for(int i = 0; i < numOfCoins; i++)
             {
                 coins[i] = read.nextInt();
             }
 
             //for(int i=0;i<=numOfCoins;i++)System.out.print(coins[i]+" ");
             refresh();
-
-
-            System.out.println(algorithm(numOfCoins,numOfSum));
-
-
+            counter=0;
+            System.out.println(countingCoins(coins, numOfCoins - 1, numOfSum));
+            System.out.println("REC Counter : "+counter);
+            counter = 0;
+            System.out.println(optimalCountingCoins(coins, numOfCoins - 1, numOfSum));
+             System.out.println("OPT Counter : "+counter);
+            
             //showMatrix(numOfCoins, numOfSum);
 
 
@@ -65,34 +68,34 @@ class Main
     {
         for(int i = 0; i < matrix.length; i++)
         {
-            if(i == 0)Arrays.fill(matrix[i], INF);
-            else
-                Arrays.fill(matrix[i], 0);
+            Arrays.fill(matrix[i], 0);
         }
     }
-    private int algorithm(int numOfCoins, int numOfSum)
+
+
+
+    private int optimalCountingCoins(int Coins[], int index, int num)
     {
+        // System.out.println(index);
+        counter++;
+        if(num == 0) return 1;
+        if(num < 0) return 0;
+        if(index < 0) return 0;
+        if(matrix[Coins[index]][num] != 0 ) return matrix[Coins[index]][num];
+        matrix[Coins[index]][num] = optimalCountingCoins(Coins, index, num - Coins[index]) + optimalCountingCoins(Coins, index - 1, num);
+        return  matrix[Coins[index]][num];
 
-        for(int i = 1; i <= numOfCoins; i++)
-        {
 
-            for(int j = 0; j <= numOfSum; j++)
-            {
-                if(coins[i] == j)matrix[coins[i]][j] = 1;
-                else
-                {
-                    if(j<coins[i])
-                    {
-                        matrix[coins[i]][j]=matrix[coins[i-1]][j];
-                    }else{
-                        matrix[coins[i]][j]=Math.min(matrix[coins[i]][j-coins[i]]+1,matrix[coins[i-1]][j]);
-                    }
+    }
 
-                }
-            }
+    private int countingCoins(int Coins[], int index, int num)
+    {
+        counter++;
+        if(num == 0)   return 1;
+        if(num < 0)    return 0;
+        if(index < 0)  return 0;
 
-        }
-        return matrix[coins[numOfCoins]][numOfSum];
+        return countingCoins(Coins, index, num - Coins[index]) + countingCoins(Coins, index - 1, num);
 
     }
 
